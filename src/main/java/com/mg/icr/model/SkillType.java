4509e2fe2da541c5b14567ee103bf4f02905f4d8
DMaliavin <dima_91@ukr.net>
2017-08-19 20:00:59 +0300

    Lombok was added

--- src/main/java/com/mg/icr/model/SkillType.java
+++ src/main/java/com/mg/icr/model/SkillType.java
@@ -1,9 +1,16 @@
 package com.mg.icr.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "skill_type", schema = "icr")
+@Setter
+@Getter
+@ToString
 public class SkillType {
 
     @Id
@@ -14,27 +21,4 @@ public class SkillType {
     @Column(name = "type", nullable = false)
     private String typeName;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTypeName() {
-        return typeName;
-    }
-
-    public void setTypeName(String typeName) {
-        this.typeName = typeName;
-    }
-
-    @Override
-    public String toString() {
-        return "SkillType{" +
-                "id=" + id +
-                ", typeName='" + typeName + '\'' +
-                '}';
-    }
 }
