e7f9ad49e26a6412ee878502685d8bb1c9876604
DMaliavin <dima_91@ukr.net>
2017-08-27 12:17:17 +0300

    Core_ExceptionHandling - Exception handler with custom exception and response type were implemented

---
+++ src/main/java/com/mg/icr/exception/ApplicationException.java
@@ -0,0 +1,16 @@
+package com.mg.icr.exception;
+
+public class ApplicationException extends RuntimeException {
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+    public ApplicationException(Throwable cause) {
+        super(cause);
+    }
+
+    public ApplicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
